52b53425450e7fdb474e3aa832614e4998435448
namecrane <francisco@namecrane.com>
2024-11-27 19:40:18 -0800

    updated email template

--- components/modules/namecrane_mail/config/namecrane_mail.php
+++ components/modules/namecrane_mail/config/namecrane_mail.php
@@ -2,22 +2,22 @@
 Configure::set('NamecraneMail.email_templates', [
    'en_us' => [
        'lang' => 'en_us',
-       'text' => 'Thanks for choosing us for your mails services!
+       'text' => 'Thanks for choosing us for your email services!
 
 Your server is now active and you can manage it through our client area by clicking the "Manage" button next to the server on your Dashboard.
 
 You may also use the following login details:
 
 Mail Login URL: <a href="https://workspace.org/">https://workspace.org/</a>
-User: {service.username}
-Pass: {service.password}
+User: {service.namecrane_mail_username}
+Password: {service.namecrane_mail_password}
 
 Thank you for your business!',
-       'html' => '<p>Thanks for choosing us for your Mail service!</p>
+       'html' => '<p>Thanks for choosing us for your email services!</p>
 <p>Your service is now active and you can manage it through our client area by clicking the "Manage" button next to the server on your Dashboard.</p>
 <p>Here are more details regarding your server:</p>
 <p>Login Page: <a href="https://workspace.org/">https://workspace.org/</a></p>
-User: {service.username}<br />Pass: {service.password}</p>
+User: {service.namecrane_mail_username}<br />Passwork: {service.namecrane_mail_password}</p>
 <p>Thank you for your business!</p>'
    ]
 ]);
